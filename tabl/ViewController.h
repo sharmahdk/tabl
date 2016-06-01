641637e9ffc846551a3e9ae38a239c3b817a725a
arthadmin <arthadmin@kushal-desai-mac-mini.local>
2016-06-01 15:13:46 +0530

    Initial Commit

---
+++ tabl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tabl
+//
+//  Created by arthadmin on 01/06/16.
+//  Copyright © 2016 arthadmin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ tabl/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  tabl
+//
+//  Created by arthadmin on 01/06/16.
+//  Copyright © 2016 arthadmin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
